92e123e8b3dbb050679d11046e9f847a6a90aaaa
SiveleleN <sivelelenkamane29@gmail.com>
2024-02-29 17:28:00 +0200

    prod

--- Model/products.js
+++ Model/products.js
@@ -21,7 +21,7 @@ class Products{
     fetchProducts(req, res){
         const qry = `
         SELECT *
-        FROM Products WHERE prodID = ?;
+        FROM Products WHERE prodID = ${req.params.id};
         `;
         db.query(qry, [req.params.id], (err, result) =>{
             if(err) throw err
